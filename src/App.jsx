ab79cf3606bb9a2587b9acd4cb67f26ada68affa
boy tillekens <boytillekens@gmail.com>
2024-04-11 14:55:19 +0200

    add more images

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import Imgs from './components/imgs.jsx'
 function App() {
   return (
     <div className="App">
+      <div className="navbar-container">
+        <div className="navbar">
+          Boy Tillekens
+        </div>
+      </div>
       <MainImg />
       <Imgs />
       <div className="text-block">
-        Boy Tillekens is a front-end developer based in The Netherlands
+      Boy Tillekens is a front-end developer with a background in animation and graphic design. Recently graduated from Le Wagon, designing and building a real-time multiplayer chess app.
       </div>
       <div className="project1">
         project here
