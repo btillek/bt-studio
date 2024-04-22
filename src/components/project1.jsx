61b521b00407c06575b6c534cdf1a0547f7b58c3
boy tillekens <boytillekens@gmail.com>
2024-04-22 09:28:01 +0200

    cleanup

--- src/components/project1.jsx
+++ src/components/project1.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import ModalNerd from './modal_nerd'
 import ModalWm from './modal_wm'
 import ModalBeacon from './modal_beacon'
